fc893e4be69941f3e940342beb511566c71a32f3
ForteScarlet <ForteScarlet@163.com>
2020-06-29 22:47:12 +0800

    add copyright

--- src/main/java/com/simplerobot/modules/utils/codeTemplate.kt
+++ src/main/java/com/simplerobot/modules/utils/codeTemplate.kt
@@ -3,11 +3,14 @@
  * Project  mod-cqcodeutils
  * File     codeTemplate.kt
  *
- *
- *
- *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
  *
  */
+
 
 package com.simplerobot.modules.utils
 
--- src/main/java/com/simplerobot/modules/utils/msg.kt
+++ src/main/java/com/simplerobot/modules/utils/msg.kt
@@ -3,9 +3,11 @@
  * Project  mod-cqcodeutils
  * File     msg.kt
  *
- *
- *
- *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
  *
  */
 
